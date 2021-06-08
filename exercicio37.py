1ab7cbefca43de36b7cd17dd31ba3ad8222b5348
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-06-08 12:58:33 -0300

    Incluíndo exercicio37.py

--- exercicio37.py
+++ exercicio37.py
@@ -2,4 +2,18 @@
 um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
 1 para binário, 2 para octal e 3 para hexadecimal."""
 
-numero = int(input('Digite um número inteiro:'))
+num = int(input('Digite um número inteiro:'))
+print ('''Escolha uma das bases para a corversão:
+[ 1 ] corverter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+
+opcao = int(input('Sua opção:'))
+if opcao == 1:
+    print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)))
+elif opcao == 2:
+    print('{} convertido para OCTAL é iglas a {}'.format(num, oct(num)))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL e igual a {}'.format(num, hex(num)))
+else:
+    print('Opção invalida, tente novamente!')
