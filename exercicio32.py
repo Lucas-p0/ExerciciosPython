6ec6fd21cb62b1a7616040eb6fae40039ce7ab33
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-06-10 22:18:40 -0300

    Correção exercicio32.py

--- exercicio32.py
+++ exercicio32.py
@@ -2,8 +2,7 @@
 
 ano = int(input('Digite um ano:'))
 
-if ano%4==0 and ano%100 !=0:
+if ano % 4 == 0 and ano % 100 != 0:
     print('{} é um ano bissexto!'.format(ano))
 else:
     print('{} não é uma ano bissexto!'.format(ano))
-
