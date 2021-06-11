a6dbf310cd0d369a8a8f1f1b93acb173f23e7bc2
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-06-11 12:58:58 -0300

    Incluíndo exercicio41.py

--- exercicio41.py
+++ exercicio41.py
@@ -6,8 +6,18 @@ de acordo com a idade:
 - Até 19 anos: JÚNIOR
 - Até 25 anos: SÊNIOR
 - Acima de 25 anos: MASTER"""
+
+#variaveis
+from datetime import date
+
 anoNascimento = int(input('Digite o ano de nascimento do atleta!:'))
-if anoNascimento < 9:
+idade =  date.today().year - anoNascimento
+print('Você tem {} anos de idade categoria:'.format(idade))
+if idade < 9:
     print('Mirim')
-elif 14 <= anoNascimento < 19:
+elif 14 <= idade < 18:
     print('infantil')
+elif 19 <= idade < 24:
+    print('Júnior')
+elif idade > 25:
+    print('Master')
