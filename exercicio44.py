42bca8b104ca1adcb218aa984551fd88c1949ccc
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-06-16 13:00:21 -0300

    Correção exercicio44.py

--- exercicio44.py
+++ exercicio44.py
@@ -10,7 +10,7 @@ print("""FORMAS DE PAGAMENTO
 [1] à vista dinheiro / cheque
 [2] à vista cartão
 [3] 2x no cartão 
-[4] 3x no cartão""")
+[4] 3x ou mais no cartão""")
 formaDePagamento = int(input('Qual a opção?:'))
 
 if formaDePagamento == 1:
@@ -18,13 +18,14 @@ if formaDePagamento == 1:
     print('Sua compra será à vista no dinheiro/cheque, com desconto de 10% o valor ficará: R${:.2f}'.format(preco))
 elif formaDePagamento == 2:
     preco = preco - (preco * 0.05)
-    print('Sua compra será à vista no cartão, com desconto de 20% o valor ficará: R${:.2f}'.format(preco))
-elif formaDePagamento == 3 or 4:
+    print('Sua compra será à vista no cartão, com desconto de 5% o valor ficará: R${:.2f}'.format(preco))
+elif formaDePagamento == 3:
+    print('O preço será :R${}'.format(preco))
+elif formaDePagamento == 4:
     parcelas = int(input('Quantas vezes no cartão?:'))
-    if parcelas == 2:
-        print('O preço será :R${}'.format(preco))
-    elif parcelas > 3:
-        precoComJuros = preco + (preco * 0.20)
-        valorDaParcela = precoComJuros / parcelas
-        print('Sua compra será parcelada em {} de {} COM JUROS.'.format(parcelas, valorDaParcela))
-        print('Sua compra de R${} vai custar R$:{} no final.'.format(preco, precoComJuros))
+    precoComJuros = preco + (preco * 0.20)
+    valorDaParcela = precoComJuros / parcelas
+    print('Sua compra será parcelada em {} de R${:.2f} COM JUROS.'.format(parcelas, valorDaParcela))
+    print('Sua compra de R${} vai custar R$:{:.2f} no final.'.format(preco, precoComJuros))
+else:
+    print('\033[1;31m OPÇÃO DE PAGAMENTO INVÁLIDA')
