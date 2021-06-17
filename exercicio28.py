45eb5dd8b5736c2a81bd02276dfa5e7b07f94f97
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-06-17 13:26:18 -0300

    Correção exercicio44.py

--- exercicio28.py
+++ exercicio28.py
@@ -1,20 +1,19 @@
 import random
 from time import sleep
 
-print('-=-'*20)
+print('-=-' * 20)
 print('Vou pensar em um numero entre 0 e 5. Tente adivinhar... ')
-print('-=-'*20)
-
-
+print('-=-' * 20)
 
 n1 = random.randrange(0, 5)
 n2 = int(input('Digite um numero:'))
 print('PROCESSANDO...')
 sleep(3)
 
-if(n1) == n2: {
+if (n1) == n2:
     print('NOSSA VOCÊ É UM VIDENTE?')
-}
-else:{
+
+else:
     print('BURRO! O numero foi:{}'.format(n1))
-}
+
+
