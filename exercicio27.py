c1357e963949a21e0500e5ef22b4546acd0b8b5f
Lucas pinheiro <lucaslogos@gmail.com>
2023-05-20 19:49:52 -0300

    update

--- exercicio27.py
+++ exercicio27.py
@@ -2,5 +2,4 @@ n = str(input('Qual o seu nome completo?:')).strip()
 nome = n.split()
 print('Seu primeiro nome é :{}'.format(nome[0]))
 print('Seu ultimo nome é : {}'.format(nome[len(nome)-1]))
-print('Seu ultimo nome é : {}'.format(len(nome)))
-
+print('Seu nome completo tem: {} palavras'.format(len(nome)))
